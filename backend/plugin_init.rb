f9241627aa260bbc51b6bf73dd5dbcf41020a57f
Mark Cooper <markchristophercooper@gmail.com>
2017-01-26 07:59:24 -0800

    Cleanup config handling in init

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -3,35 +3,36 @@ Dir.glob(File.join(File.dirname(__FILE__), "lib", "*.rb")).sort.each do |file|
   require File.absolute_path(file)
 end
 
-# default converter tree (should match what aspace provides)
-# can be overriden in config.rb
-AppConfig[:converter_tree] = {
-  accession: {
-    csv: {
-      converter_class: AccessionConverter,
-      parse_method: :parse_as_csv,
+unless AppConfig.has_key?(:converter_tree)
+  # default converter tree (should match what aspace provides)
+  AppConfig[:converter_tree] = {
+    accession: {
+      csv: {
+        converter_class: AccessionConverter,
+        parse_method: :parse_as_csv,
+      },
     },
-  },
-  agent: {
-    eac: {
-      converter_class: EACConverter,
-      parse_method: :parse_as_xml
+    agent: {
+      eac: {
+        converter_class: EACConverter,
+        parse_method: :parse_as_xml
+      },
     },
-  },
-  digital_object: {
-    csv: {
-      converter_class: DigitalObjectConverter,
-      parse_method: :parse_as_csv,
+    digital_object: {
+      csv: {
+        converter_class: DigitalObjectConverter,
+        parse_method: :parse_as_csv,
+      },
     },
-  },
-  resource: {
-    ead: {
-      converter_class: EADConverter,
-      parse_method: :parse_as_xml,
+    resource: {
+      ead: {
+        converter_class: EADConverter,
+        parse_method: :parse_as_xml,
+      },
+      marcxml: {
+        converter_class: MarcXMLConverter,
+        parse_method: :parse_as_xml,
+      },
     },
-    marcxml: {
-      converter_class: MarcXMLConverter,
-      parse_method: :parse_as_xml,
-    },
-  },
-}
+  }
+end
